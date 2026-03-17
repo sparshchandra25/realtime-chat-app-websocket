01e0105ecbadea38394236794fc0906afe3bd3d2
sparshchandra25 <sparshcollege25@gmail.com>
2026-03-17 22:58:52 +0530

    fixed duplicate websocket

--- server.js
+++ server.js
@@ -2,25 +2,7 @@ const WebSocket = require('ws');
 const http = require('http');
 
 const server = http.createServer();
-
 const wss = new WebSocket.Server({ server });
-
-wss.on('connection', function connection(ws) {
-    console.log("New client connected");
-
-    ws.on('message', function incoming(message) {
-        wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message.toString());
-            }
-        });
-    });
-});
-
-// 🔥 THIS LINE IS THE IMPORTANT PART
-server.listen(process.env.PORT || 3000, () => {
-    console.log("Server running...");
-});
 
 wss.on('connection', function connection(ws) {
     console.log("New client connected");
@@ -28,7 +10,7 @@ wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log('Received:', message.toString());
 
-        // Broadcast message to all clients
+        // Broadcast to all clients
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message.toString());
@@ -39,4 +21,8 @@ wss.on('connection', function connection(ws) {
     ws.on('close', () => {
         console.log("Client disconnected");
     });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log("Server running...");
 });
